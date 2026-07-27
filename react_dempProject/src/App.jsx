8b7b4e102e8cad6a1cfb7b7cc5bb2138ce60fb3d
DurgeshKhairnar <2002durgeshk@gmail.com>
2026-07-27 22:51:42 +0530

    first commit

---
+++ react_dempProject/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useState} from 'react'
+import Card from './Card.jsx'
+function App(){
+   let name = "Mukesh"
+    return (
+         <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
+            
+         </div>
+    );
+}
+
+export default App
---
+++ react_dempProject/src/Card.jsx
@@ -0,0 +1,11 @@
+
+
+function Card({ name = "Durgesh" }){
+    return (
+       <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
+        
+       </div>
+    );
+}
+
+export default Card
