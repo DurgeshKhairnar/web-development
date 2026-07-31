a8e571e3be765ef853bc1ed619384d50aa2ab36c
DurgeshKhairnar <2002durgeshk@gmail.com>
2026-07-31 10:45:16 +0530

    card

--- react_dempProject/Card/src/App.tsx
+++ react_dempProject/Card/src/App.tsx
@@ -1,20 +1,9 @@
 import Card from "./Card.tsx";
 function App(){
-  const info = [
-    {
-      title:"Apple 1",
-      des:"Apple is good fruite for health"
-    },
-    {
-      title:"Apple 2",
-      des:"Apple color is red"
-    }
-  ]
+
   return (
    <div className="container">
-        {
-          info.map((item,index) => <Card  obj={item} key={index}/>)
-        }
+        <Card />
    </div>
   )
 }
--- react_dempProject/Card/src/Card.tsx
+++ react_dempProject/Card/src/Card.tsx
@@ -1,14 +1,36 @@
-function Card(props: any){
-     console.log(props.obj.title)
-  return (
-      <div className="parent" >
-          <div className="image"></div>
-          <h3>{props.obj.title}</h3>
-          <p>{props.obj.des}</p>
-          <button>Action</button>
-      </div>
-  )
-}
+import { useState } from "react";
 
+function Card() {
+  const [addTask, setTask] = useState('');
+  const [task, updateTask] = useState<string[]>([]);
+  
+
+
+  function addTaskFun(e:any){
+    e.preventDefault();
+    
+    updateTask([...task,addTask]);
+    setTask('');
+  }
+
+  return (
+    <div className="parent">
+           <div className="container">
+                <form onSubmit={addTaskFun}>
+                  <input type='text' placeholder='Add new task'
+                  value={addTask}
+                  onChange={(e) => setTask(e.target.value)}
+                  ></input>
+                 <button >ADD</button>
+                </form>
+           </div>
+            <div className='task-display'>
+                   {
+                        task.map((data,index)=> <h4 className="task-container" key={index}>{data}</h4>)
+                   }
+            </div>
+    </div>
+  );
+}
 
 export default Card;
