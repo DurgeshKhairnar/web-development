c880fad7e0771e9a9aef2e0a4f65564028ff1a40
DurgeshKhairnar <2002durgeshk@gmail.com>
2026-08-16 17:27:20 +0530

    teailwindcss practices

--- react_dempProject/Card/src/App.tsx
+++ react_dempProject/Card/src/App.tsx
@@ -3,9 +3,11 @@ import CardCounter from './CardCounter.tsx'
 function App(){
 
   return (
-   <div className="container">
-        <CardCounter />
-   </div>
+      <div className="w-50 h-50 border-2 border-blue-500 rounded-3xl flex flex-col justify-between">
+          <p>Durgesh</p>
+          <p>jayesh</p>
+          <p>ganesh</p>
+      </div>
   )
 }
 
---
+++ react_dempProject/myScript.js
@@ -0,0 +1,75 @@
+
+//  let user1 = {
+//     name:"Durgesh",
+//     greet:function(){
+//         console.log(this.name)
+//     }
+//  }
+
+//   let user2 = {
+//     name:"sai",
+//     nam2:user1.name,
+//     greet:user1.greet
+//  }
+
+// user1.greet()
+// user2.greet()
+
+// function test() {
+//   console.log(this);
+// }
+
+// test();
+
+
+// let obj = {
+//     name : "durgesh",
+//     greet : ()  => {
+//         console.log(this)
+//     }
+// }
+
+// obj.greet();
+
+
+// const user = {
+//     name: "Durgesh",
+
+//     normalFunction: function () {
+//         let name = "sai"
+//         console.log(name);
+
+//         const arrowFunction = () => {
+//             console.log(this.name);
+//         };
+
+//         arrowFunction();
+//     }
+// };
+
+// user.normalFunction();
+
+
+
+
+
+const user = {
+    name: "Durgesh",
+
+    normalFunction: function () {
+        console.log(this.name);
+
+        const arrowFunction = () => {
+            console.log(this.name);
+
+            const arrFun = () => {
+                 console.log(this.name);
+            }
+            arrFun()
+        };
+
+        arrowFunction();
+    }
+};
+
+user.normalFunction();
