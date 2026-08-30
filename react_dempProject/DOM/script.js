fefd99294c49d684a2cfa1ae6dc5a1f8e6324aa0
DurgeshKhairnar <2002durgeshk@gmail.com>
2026-08-30 18:19:44 +0530

    dom p

--- react_dempProject/DOM/script.js
+++ react_dempProject/DOM/script.js
@@ -1,14 +1,21 @@
 
 
-let main = document.createElement('main')
-let div = document.createElement('div');
-div.classList.add('outerDiv');
-div.style.backgroundColor = 'red';
-div.style.height = '500px';
-div.style.width = '500px';
-main.appendChild(div);
-document.body.appendChild(main);
+// let main = document.createElement('main')
+// let div = document.createElement('div');
+// div.classList.add('outerDiv');
+// div.style.backgroundColor = 'red';
+// div.style.height = '500px';
+// div.style.width = '500px';
+// main.appendChild(div);
+// document.body.appendChild(main);
 
 
+let input = document.querySelector('input');
+let submit = document.querySelector('button');
+let h3 = document.querySelector('h3')
 
+submit.addEventListener('click',() => {
+    h3.textContent = input.value;
+    input.value = '';
+})
 
